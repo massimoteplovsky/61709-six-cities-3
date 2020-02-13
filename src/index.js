09a57c0e57061d131792e028cb98ca8b709812cc
maXimalist <www.maximalist@mail.ru>
2020-02-13 16:24:50 +0300

    First components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app.jsx';
 
+const offersCount = 5;
+
+ReactDOM.render(
+    <App
+      offersCount={offersCount}
+    />,
+    document.querySelector(`#root`)
+);
